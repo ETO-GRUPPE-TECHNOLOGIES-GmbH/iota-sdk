7a139a81c2b3adb54ccb7d44ec4bb39f1432d7c2
Anton Berneving <40672068+antbern@users.noreply.github.com>
2024-11-14 08:12:20 +0100

    enable WASM StorageAdapter to also work in mocked `nodejs` (#1)
    * enable and update WASM StorageAdapter
    
    * delete probe key when done
    
    * cleanup test feature flag
    
    * commit Cargo.lock changes
    
    * move the probe constants into where they are used

--- sdk/src/wallet/storage/adapter/wasm.rs
+++ sdk/src/wallet/storage/adapter/wasm.rs
@@ -1,7 +1,9 @@
 // Copyright 2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use gloo_storage::LocalStorage;
+use web_sys::{js_sys, wasm_bindgen::JsValue};
+
+use crate::wallet::Error;
 
 use super::StorageAdapter;
 
@@ -10,35 +12,81 @@ pub const STORAGE_ID: &str = "Wasm";
 
 /// Wasm storage adapter using the browser local storage
 #[derive(Debug)]
-pub struct WasmAdapter(LocalStorage);
+pub struct WasmAdapter {
+    /// field used just to make it impossible to create a [`WasmAdapter`] without using the `new` function.
+    _ignored: (),
+}
 
 impl WasmAdapter {
-    /// Initialises the storage adapter.
+    /// Tries to instantiate a new [`WasmAdapter`] by checking if the local storage API is
+    /// available with a simple write-read cycle.
     pub fn new() -> crate::wallet::Result<Self> {
-        Ok(Self(LocalStorage::new()))
+        let storage = Self::storage()?;
+
+        // do a write-read cycle, and if any of them fail, wrap the error and return it
+        let out = || -> Result<bool, JsValue> {
+            const PROBE_KEY: &str = "iota-sdk-availability-test";
+            const PROBE_VALUE: &str = "probe_value";
+
+            storage.set_item(PROBE_KEY, PROBE_VALUE)?;
+            let read = storage.get_item(PROBE_KEY)?;
+            let _ = storage.remove_item(PROBE_KEY);
+            Ok(read.is_some_and(|v| v == PROBE_VALUE))
+        }();
+
+        let matched = out.map_err(|e| Error::Storage(format!("localStorage probe check failed with error: {e:?}")))?;
+
+        if !matched {
+            return Err(Error::Storage(
+                "localStorage probe check failed: written and read values do not match!".to_string(),
+            ));
+        }
+
+        Ok(Self { _ignored: () })
+    }
+
+    /// Use reflection instead of the `window` object to get hold of a reference to the local storage API.
+    /// This makes sure we can utilize it when running in nodejs with a mocked global
+    /// `window.localStorage` object.
+    fn storage() -> crate::wallet::Result<web_sys::Storage> {
+        let window_obj = js_sys::Reflect::get(&js_sys::global(), &JsValue::from_str("window"))
+            .map_err(|e| Error::Storage(format!("no window object found: {e:?}")))?;
+
+        let local_storage_obj = js_sys::Reflect::get(&window_obj, &JsValue::from_str("localStorage"))
+            .map_err(|e| Error::Storage(format!("no window.localStorage object found: {e:?}")))?;
+
+        let storage = web_sys::Storage::try_from(local_storage_obj)
+            .map_err(|e| Error::Storage(format!("window.localStorage should be web_sys::Storage: {e}")))?;
+        Ok(storage)
     }
 }
 
 #[async_trait::async_trait]
 impl StorageAdapter for WasmAdapter {
+    type Error = crate::wallet::Error;
+
     /// Gets the record associated with the given key from the storage.
-    async fn get(&self, key: &str) -> crate::wallet::Result<String> {
-        self.0.get(key)
+    async fn get_bytes(&self, key: &str) -> Result<Option<Vec<u8>>, Self::Error> {
+        Ok(Self::storage()?
+            .get_item(key)
+            .map_err(|e| Error::Storage(format!("get_item error: {e:?}")))?
+            .map(|s| s.into_bytes()))
     }
 
     /// Saves or updates a record on the storage.
-    async fn set(&mut self, key: &str, record: String) -> crate::wallet::Result<()> {
-        self.0.set(key, record)
-    }
+    async fn set_bytes(&self, key: &str, record: &[u8]) -> Result<(), Self::Error> {
+        // We always store valid UTF8 JSON, so this does not loose any information
+        let record = String::from_utf8_lossy(record);
 
-    /// Batch writes records to the storage.
-    async fn batch_set(&mut self, records: HashMap<String, String>) -> crate::wallet::Result<()> {
-        records.into_iter().map(|s| self.set(s.0, s.1));
-        Ok(())
+        Self::storage()?
+            .set_item(key, &record)
+            .map_err(|e| Error::Storage(format!("set_item error: {e:?}")))
     }
 
     /// Removes a record from the storage.
-    async fn remove(&mut self, key: &str) -> crate::wallet::Result<()> {
-        self.0.delete(key)
+    async fn delete(&self, key: &str) -> crate::wallet::Result<()> {
+        Self::storage()?
+            .delete(key)
+            .map_err(|e| Error::Storage(format!("delete error: {e:?}")))
     }
 }
